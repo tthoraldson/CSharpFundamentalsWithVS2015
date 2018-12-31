15ed5d495f1ec77c3fc739dfe5837c5efbb3db33
tthoraldson <theresa.thoraldson@outlook.com>
2018-12-31 10:21:17 -0600

    exercise4 files

---
+++ exercise4/Grades.Test/Types/ReferenceTypeTests.cs
@@ -0,0 +1,32 @@
+﻿using exercise4;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Grades.Test.Types
+{
+    [TestClass]
+    public class ReferenceTypeTests
+    {
+        [TestMethod]
+        public void IntVariablesHoldAValue()
+        {
+            int x1 = 100;
+            int x2 = x1;
+
+            x1 = 4;
+            Assert.AreNotEqual(x1, x2);
+        }
+        [TestMethod]
+        public void GradeBookVariablesHoldAReference()
+        {
+            GradeBook g1 = new GradeBook();
+            GradeBook g2 = g1;
+
+            g1 = new GradeBook();
+            g1.Name = "Theresa's grade book";
+            Console.WriteLine(g2.Name);
+
+            Assert.AreEqual(g1.Name, g2.Name);
+        }
+    }
+}
---
+++ exercise4/exercise4/GradeBook.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise4
+{
+    public class GradeBook
+    {
+        public GradeBook()
+        {
+            grades = new List<float>();
+        }
+
+        public GradeStatistics ComputeStatistics()
+        {
+            GradeStatistics stats = new GradeStatistics();
+            stats.HighestGrade = 0;
+
+            float sum = 0;
+            foreach (float grade in grades)
+            {
+                stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
+                stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
+                sum += grade;
+            }
+
+            stats.AverageGrade = sum / grades.Count;
+            return stats;
+        }
+
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+
+        public string Name;
+        public List<float> grades = new List<float>();
+    }
+}
